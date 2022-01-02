8139d63f5fc701a28c6e08cee757b56e0e7475e2
Shoothzj <shoothzj@gmail.com>
2022-01-03 00:23:56 +0800

    print detail log when failure

--- go.mod
+++ go.mod
@@ -1,10 +1,14 @@
 module rabbitmq_mate_go
 
-go 1.16
+go 1.17
 
 require (
-	github.com/paashzj/gutil v0.0.2 // indirect
+	github.com/paashzj/gutil v0.0.9
+	go.uber.org/zap v1.19.1
+)
+
+require (
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.7.0 // indirect
-	go.uber.org/zap v1.19.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- pkg/rabbit/start.go
+++ pkg/rabbit/start.go
@@ -1,19 +1,23 @@
 package rabbit
 
 import (
+	"github.com/paashzj/gutil"
+	"go.uber.org/zap"
 	"rabbitmq_mate_go/pkg/path"
 	"rabbitmq_mate_go/pkg/util"
 	"time"
 )
 
 func Start() error {
-	err := util.CallScript(path.RabbitStartScript)
+	stdout, stderr, err := gutil.CallScript(path.RabbitStartScript)
+	util.Logger().Info("shell result ", zap.String("stdout", stdout), zap.String("stderr", stderr))
 	if err != nil {
-		util.Logger().Error("start rabbit failed")
+		util.Logger().Error("command wait error ", zap.Error(err))
 		return err
 	}
-	time.Sleep(5 * time.Second)
-	err = util.CallScript(path.RabbitInitUserScript)
+	time.Sleep(10 * time.Second)
+	stdout, stderr, err = gutil.CallScript(path.RabbitInitUserScript)
+	util.Logger().Info("shell result ", zap.String("stdout", stdout), zap.String("stderr", stderr))
 	if err != nil {
 		util.Logger().Error("rabbit init user failed")
 	}
