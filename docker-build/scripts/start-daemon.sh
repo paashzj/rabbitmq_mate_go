a3f1562d3ac5550dd43fe6f5f70ddac62186325e
lovehzj <1922919664@qq.com>
2022-01-09 14:49:34 +0800

    fix mate log path

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-nohup $RABBITMQ_HOME/mate/rabbitmq_mate >$RABBITMQ_HOME/rabbitmq_mate.stdout.log 2>$RABBITMQ_HOME/rabbitmq_mate.stderr.log
+mkdir $RABBITMQ_HOME/logs
+nohup $RABBITMQ_HOME/mate/rabbitmq_mate >>$RABBITMQ_HOME/logs/rabbitmq_mate.stdout.log 2>>$RABBITMQ_HOME/rabbitmq_mate.stderr.log
+
