2e1d3dee303477b8211e5beef9d532fd6767d2c0
lovehzj <1922919664@qq.com>
2022-01-14 07:19:10 +0800

    Release docker image binary

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir $RABBITMQ_HOME/logs
+mkdir -p $RABBITMQ_HOME/logs
 nohup $RABBITMQ_HOME/mate/rabbitmq_mate >>$RABBITMQ_HOME/logs/rabbitmq_mate.stdout.log 2>>$RABBITMQ_HOME/rabbitmq_mate.stderr.log
 
